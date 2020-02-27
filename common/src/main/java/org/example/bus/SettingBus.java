00a62b819c2e846300b54b72154bcf3debb6f5c5
lixinyang <741162208@qq.com>
2020-02-27 17:25:25 +0800

    给setting做缓存

--- common/src/main/java/org/example/bus/SettingBus.java
+++ common/src/main/java/org/example/bus/SettingBus.java
@@ -1,10 +1,23 @@
 package org.example.bus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.example.bus.Constants.IMPL_SUFFIX;
 
 public class SettingBus {
 
+    private static final Map<String, Object> SETTING_CLASS_CACHE = new HashMap<String, Object>();
+
     public static <T> T obtainSetting(Class<T> clazz) {
+        String clazzName = clazz.toString();
+        if (!SETTING_CLASS_CACHE.containsKey(clazzName)) {
+            SETTING_CLASS_CACHE.put(clazzName, findSettingsImpl(clazz));
+        }
+        return (T) SETTING_CLASS_CACHE.get(clazz.toString());
+    }
+
+    private static <T> T findSettingsImpl(Class<T> clazz) {
         try {
             String interfaceName = clazz.getCanonicalName();
             System.out.println(interfaceName);
