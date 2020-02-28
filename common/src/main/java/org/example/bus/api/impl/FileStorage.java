7ba0d114ce00d76572ba377843e070bd530f9558
lixinyang <741162208@qq.com>
2020-02-29 01:34:11 +0800

    fileStorage换一种实现

--- common/src/main/java/org/example/bus/api/impl/FileStorage.java
+++ common/src/main/java/org/example/bus/api/impl/FileStorage.java
@@ -7,17 +7,25 @@ import java.util.Map;
 
 public class FileStorage implements LocalSettingStorage {
 
-    private static final String FILE_PATH = "./";
+    private String filePath;
+
+    public FileStorage() {
+        filePath = "./local_setting_storage.txt";
+    }
+
+    public FileStorage(String path) {
+        filePath = path;
+    }
 
     @Override
     public void save(Map<String, String> kvs) {
-        FileUtil.ensureFileExists(FILE_PATH);
-        FileUtil.writeToFile(kvs, FILE_PATH);
+        FileUtil.ensureFileExists(filePath);
+        FileUtil.writeToFile(kvs, filePath);
     }
 
     @Override
     public Map<String, String> read() {
-        FileUtil.ensureFileExists(FILE_PATH);
-        return FileUtil.readFromFile(FILE_PATH);
+        FileUtil.ensureFileExists(filePath);
+        return FileUtil.readFromFile(filePath);
     }
 }
