e1614929b205cef91a49069da6dcc11d5f102f36
lixinyang <741162208@qq.com>
2020-03-16 02:56:36 +0800

    小优化

--- demo/src/main/java/org/example/demo/test/TestMain.java
+++ demo/src/main/java/org/example/demo/test/TestMain.java
@@ -5,13 +5,8 @@ import static org.example.demo.test.TestRemote.testRemote;
 
 public class TestMain {
 
-
-
     public static void main(String[] args) {
         testLocal();
-
         testRemote();
     }
-
-
 }
