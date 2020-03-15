aa2e1b601e902fd4178c892a9f3e8d182f033afc
lixinyang <741162208@qq.com>
2020-03-16 02:55:28 +0800

    消除警告并进行小优化

--- common/src/main/java/org/example/bus/SettingBus.java
+++ common/src/main/java/org/example/bus/SettingBus.java
@@ -10,14 +10,16 @@ public class SettingBus {
 
     private static final Map<String, Object> SETTING_IMPL_CLASS_CACHE = new HashMap<String, Object>();
 
+    @SuppressWarnings("unchecked")
     public static <T> T obtainSetting(Class<T> clazz) {
-        String clazzName = clazz.toString();
+        String clazzName = clazz.getCanonicalName();
         if (!SETTING_IMPL_CLASS_CACHE.containsKey(clazzName)) {
             SETTING_IMPL_CLASS_CACHE.put(clazzName, lookForSettingsImpl(clazz));
         }
-        return (T) SETTING_IMPL_CLASS_CACHE.get(clazz.toString());
+        return (T) SETTING_IMPL_CLASS_CACHE.get(clazzName);
     }
 
+    @SuppressWarnings("unchecked")
     private static <T> T lookForSettingsImpl(Class<T> clazz) {
         try {
             String interfaceName = clazz.getCanonicalName();
