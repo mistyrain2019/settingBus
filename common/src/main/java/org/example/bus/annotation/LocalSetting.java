af1b691fdd6a7b3858221a6690d8d7d6f9a723aa
lixinyang <741162208@qq.com>
2020-04-11 23:23:16 +0800

    限定注解的被注解对象

--- common/src/main/java/org/example/bus/annotation/LocalSetting.java
+++ common/src/main/java/org/example/bus/annotation/LocalSetting.java
@@ -1,5 +1,10 @@
 package org.example.bus.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 可从本地读取和修改的设置
  * <p>
@@ -10,5 +15,7 @@ package org.example.bus.annotation;
  * @see SettingGetter  或者
  * @see SettingSetter  所注解的
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
 public @interface LocalSetting {
 }
